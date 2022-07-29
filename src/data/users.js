a583ee9d0d8ce8b33a772479194300a9a0cc67c3
Victor Campos <victor-campos@outlook.com>
2022-07-29 15:36:20 -0400

    config 20220729 1536

--- src/data/users.js
+++ src/data/users.js
@@ -1,4 +1,5 @@
 export const ListUsers = [
     { username: "victorcampos", password: "senha1", email: "victor-campos@outlook.com" }
     , { username: "brunocampos", password: "senha2", email: "bruno-campos@outlook.com" }
+    , { username: "renato", password: "123456", email: "icms@escritoriosorriso.com.br" }
 ];
